d2104e634984002db29cefaf2360444f222c7d94
seaeyedwolf <seaeyedwolf@gmail.com>
2019-12-19 21:09:54 +0300

    Final commit

--- client.py
+++ client.py
@@ -2,21 +2,21 @@ import asyncio
 
 
 HOST = 'localhost'
-PORT = 9095
+PORT = 9090
 
 
 async def tcp_echo_client(host, port):
     reader, writer = await asyncio.open_connection(host, port)
-    message = 'Hello, world'
+    message = input("Enter message: ")
 
     writer.write(message.encode())
     await writer.drain()
 
     data = await reader.read(100)
+    print(data.decode())
     writer.close()
-    # await writer.wait_closed()
+    await writer.wait_closed()
 
-# asyncio.run(tcp_echo_client(HOST, PORT))
 
 loop = asyncio.get_event_loop()
 task = loop.create_task(tcp_echo_client(HOST, PORT))
